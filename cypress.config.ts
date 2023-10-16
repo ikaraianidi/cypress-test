d2395e19e2f71e0316dcbb75b940dd6486f05d75
Ihor Karaianidi <karayanidi.igor@gmail.com>
2023-10-16 18:28:29 +0300

    Add mochawesome report with corresponding scripts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   viewportHeight: 1080,
   viewportWidth: 1920,
   e2e: {
